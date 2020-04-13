04b2e9f6ee8597b0d043ff3848f26d7e9d1558ae
Adam-Harte <adamharte11@hotmail.co.uk>
2020-04-13 12:51:19 +0100

    JSON response data added

--- app.js
+++ app.js
@@ -3,4 +3,12 @@ const xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 
+// JSON data
+// xhr.responseType = 'json';
+
+xhr.onload = function () {
+	console.log(xhr.response);
+	console.log(JSON.parse(xhr.response));
+};
+
 xhr.send();
